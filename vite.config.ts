74971bbb9a79300234a1aabb881ea0a6c0a5e10c
Kanzoon Rekza <kanzoonrkz@gmail.com>
2024-12-17 14:10:24 +0700

    init tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    // ...,
+  ],
+});
